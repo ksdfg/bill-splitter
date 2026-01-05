c2020a53dd329021728252bb14f45f3348895c80
Kshitish Deshpande <kshitish.deshpande@one2n.in>
2026-01-05 14:25:40 +0530

    test: add fixture for fastapi test client

--- backend/tests/api/v1/test_health_api_v1.py
+++ backend/tests/api/v1/test_health_api_v1.py
@@ -1,11 +1,4 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-test_client = TestClient(app)
-
-
-def test_health_check():
+def test_health_check(test_client):
     response = test_client.get("/api/v1/health/")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
